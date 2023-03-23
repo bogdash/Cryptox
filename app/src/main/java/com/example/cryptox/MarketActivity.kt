4da40e460092fdf8edfd24b4dcde30af042be3db
Anastasia <ms.anastasia.bogdanova@gmail.com>
2023-03-23 17:24:56 +0300

    add market activity

---
+++ app/src/main/java/com/example/cryptox/MarketActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cryptox
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MarketActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_market)
+    }
+}
--- app/src/main/java/com/example/cryptox/SignUpActivity.kt
+++ app/src/main/java/com/example/cryptox/SignUpActivity.kt
@@ -1,13 +1,22 @@
 package com.example.cryptox
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SignUpActivity: AppCompatActivity() {
 
+    private lateinit var buttonSignUp: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_sign_up)
+        buttonSignUp = findViewById(R.id.buttonSignUp)
+
+        buttonSignUp.setOnClickListener {
+            val intent = Intent(this@SignUpActivity, MarketActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

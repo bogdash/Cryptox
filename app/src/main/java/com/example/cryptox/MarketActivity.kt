5c377dd43db2ab7b147d3c9b3b7ee9b6e770b22a
Anastasia <ms.anastasia.bogdanova@gmail.com>
2023-03-23 19:22:33 +0300

    add toolbar and menu

--- app/src/main/java/com/example/cryptox/MarketActivity.kt
+++ app/src/main/java/com/example/cryptox/MarketActivity.kt
@@ -1,6 +1,7 @@
 package com.example.cryptox
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 
 class MarketActivity : AppCompatActivity() {
@@ -8,5 +9,12 @@ class MarketActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_market)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+       menuInflater.inflate(R.menu.menu, menu)
+        return true
     }
 }

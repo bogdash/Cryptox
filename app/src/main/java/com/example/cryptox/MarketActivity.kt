070bdbf63d5be92e3c929ee0d0b7f349ef43c52b
Anastasia <ms.anastasia.bogdanova@gmail.com>
2023-04-03 13:11:59 +0300

    add behavior to menu

--- app/src/main/java/com/example/cryptox/MarketActivity.kt
+++ app/src/main/java/com/example/cryptox/MarketActivity.kt
@@ -3,18 +3,68 @@ package com.example.cryptox
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.cryptox.databinding.ActivityMarketBinding
 
 class MarketActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMarketBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_market)
-
         setSupportActionBar(findViewById(R.id.toolbar))
+        binding = ActivityMarketBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        loadFragment(HomeFragment.newInstance())
+        binding.bottomNavigationView.setOnItemReselectedListener { item ->
+            var fragment: Fragment
+            when (item.itemId) {
+                R.id.home_item -> {
+                    binding.toolbar.title = "Home"
+                    fragment = HomeFragment()
+                    loadFragment(fragment)
+                    true
+                }
+                R.id.trends_item -> {
+                    binding.toolbar.title = "Trends"
+                    fragment = TrendsFragment()
+                    loadFragment(fragment)
+                    true
+                }
+                R.id.market_item -> {
+                    binding.toolbar.title = "Market"
+                    fragment = MarketFragment()
+                    loadFragment(fragment)
+                    true
+                }
+                R.id.qr_scanner_item -> {
+                    binding.toolbar.title = "QRScanner"
+                    fragment = QRScannerFragment()
+                    loadFragment(fragment)
+                    true
+                }
+                R.id.user_item -> {
+                    binding.toolbar.title = "User"
+                    fragment = UserFragment()
+                    loadFragment(fragment)
+                    true
+                }
+                else -> false
+            }
+
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, fragment)
+            .commit()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-       menuInflater.inflate(R.menu.menu, menu)
+        menuInflater.inflate(R.menu.menu, menu)
         return true
     }
 }
